a7f3e749e247015faf55faa7dd868bf88583d034
tanjilulkarim <tanjilkarim580@gmail.com>
2021-10-07 07:38:37 +0600

    first commmit

---
+++ src/components/Inventory/InventoryPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InventoryPage = () => {
+    return (
+        <div>
+            <h2>this is inventory</h2>
+        </div>
+    );
+};
+
+export default InventoryPage;
